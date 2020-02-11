a00bd64f91ba98e77226dac9b294959bfe57a18b
Le Hong Phuong <helloitpdu@gmail.com>
2020-02-11 12:43:38 +0700

    PhuongLH5: commit all file at 02/11/2020 : 12:43:37.39

--- herokuapp/admin.py
+++ herokuapp/admin.py
@@ -4,8 +4,8 @@ from import_export.admin import ImportExportModelAdmin
 from . import models
 
 class CustomerAdmin(ImportExportModelAdmin):
-    list_display = ('id','cmnd','username','phone','number_ticket','deposit','money','start_date','start_time_train','end_date','end_time_train','note','status')
-    list_filter = ['id','cmnd','username','phone','number_ticket','deposit','money','start_date','start_time_train','end_date','end_time_train','note','status']
-    search_fields = ['id','cmnd','username','phone','number_ticket','deposit','money','start_date','start_time_train','end_date','end_time_train','note','status'] 
+    list_display = ('id','cmnd','username','phone','number_ticket','number_ticket_go','number_ticket_return','deposit','money','start_date','start_time_train','end_date','end_time_train','note','status')
+    list_filter = ['id','cmnd','username','phone','number_ticket','number_ticket_go','number_ticket_return','deposit','money','start_date','start_time_train','end_date','end_time_train','note','status']
+    search_fields = ['id','cmnd','username','phone','number_ticket','number_ticket_go','number_ticket_return','deposit','money','start_date','start_time_train','end_date','end_time_train','note','status'] 
  
 admin.site.register(models.Customer, CustomerAdmin)
--- herokuapp/models.py
+++ herokuapp/models.py
@@ -12,18 +12,24 @@ STATUS_BOOK_TICKET = [
     (3, 'Vé Không Đi')
 ]
 
+
 class Customer(models.Model):
     id = models.AutoField
     cmnd = models.IntegerField('CMND', default=0)
     username = models.CharField('Tên Khách Hàng', max_length=50)
     phone = models.CharField('Số Điện Thoại', max_length=30)
-    number_ticket = models.CharField('Số Vé Mua', max_length=50)
+    number_ticket = models.IntegerField('Số Vé Mua', default=0)
+    number_ticket_go = models.IntegerField('Số Vé Đi', default=0)
+    number_ticket_return = models.IntegerField('Số Vé Về', default=0) 
     deposit = models.CharField('Tiền Đặt Cọc', max_length=50)
     money = models.CharField('Tiền Còn Thiếu', max_length=50)
     create_date = models.DateField('Ngày Nhập', default=datetime.date.today)
     start_date = models.DateField('Ngày Tàu Đi', blank=True, null=True)
-    start_time_train = models.TimeField('Giờ Đi', max_length=50, blank=True, null=True)
+    start_time_train = models.TimeField(
+        'Giờ Đi', max_length=50, blank=True, null=True)
     end_date = models.DateField('Ngày Tàu Về', blank=True, null=True)
-    end_time_train = models.TimeField('Giờ Về', max_length=50, blank=True, null=True)
+    end_time_train = models.TimeField(
+        'Giờ Về', max_length=50, blank=True, null=True)
     note = models.TextField('Ghi Chú', max_length=2000)
-    status = models.IntegerField('Trạng Thái', default=0, choices=STATUS_BOOK_TICKET) 
+    status = models.IntegerField(
+        'Trạng Thái', default=0, choices=STATUS_BOOK_TICKET)
